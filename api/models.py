2608f9dd004530b6151e111b6bcadeb718373351
Pavel <xoste49@gmail.com>
2021-06-02 14:03:31 +0300

    Add model Review

--- api/models.py
+++ api/models.py
@@ -1,3 +1,40 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+    # review_id
+    # title_id объект для оценки
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    # string (Текст отзыва)
+    text = models.CharField(
+        "Текст отзыва",
+        max_length=1500,
+        help_text='Максимальная длина 1500 символов'
+    )
+    # string (username пользователя)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    # integer (Оценка) [ 1 .. 10 ]
+    score = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    # string <date-time> (Дата публикации отзыва)
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
