301105a8f8212598054c2b140987be55aaae96d6
Pavel <xoste49@gmail.com>
2021-06-02 15:25:55 +0300

    Add ReviewViewSet and CommentViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import filters, viewsets
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from .models import Review, Comment, Title
+from .serializers import ReviewSerializer, CommentSerializer
+
+User = get_user_model()
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    #queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    #permission_classes = [IsAuthorOrReadOnly]
+    #filter_backends = [DjangoFilterBackend]
+    #filterset_fields = ['group', ]
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    #permission_classes = [IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
